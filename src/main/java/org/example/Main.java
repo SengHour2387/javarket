4a5059c1fbff5772658530f90e19af5b5f23f60c
Shawnnnnhh <100029619+Shawnnnnhh@users.noreply.github.com>
2025-10-03 12:42:10 +0700

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.authenScreens.AuthenScreen;
+import org.example.screens.MainFrame;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
